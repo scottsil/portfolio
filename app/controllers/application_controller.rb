41455198a1feb6f2e3478d43e614ca40e52b0ac2
Scott Silverman <scottsil@me.com>
2011-10-10 17:33:57 -0700

    order projects by order_index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def index
-    @projects = Project.all
+    @projects = Project.order(:order_index).all
   end
   
 end
