b73eec91159dace4371366e77a6e48b4c433061b
Scott Silverman <scottsil@me.com>
2011-10-12 01:07:01 -0700

    change migration for heroku

--- db/migrate/20111010083632_add_order_index_to_projects.rb
+++ db/migrate/20111010083632_add_order_index_to_projects.rb
@@ -1,6 +1,5 @@
 class AddOrderIndexToProjects < ActiveRecord::Migration
   def change
-    remove_column :projects, :order_index
     add_column :projects, :order_index, :integer, :default => nil
     Project.find_each do |project|
       project.update_attribute(:order_index, nil)
