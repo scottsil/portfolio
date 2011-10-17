b2215ae54ff9a1af694e682fffd2ad329802697d
Scott Silverman <scottsil@me.com>
2011-10-17 00:04:11 -0700

    migration to change description to text and remove 255 char limit

---
+++ db/migrate/20111017062316_remove_limit_from_project_description.rb
@@ -0,0 +1,9 @@
+class RemoveLimitFromProjectDescription < ActiveRecord::Migration
+  def up
+    change_column :projects, :description, :text, :limit => nil
+  end
+
+  def down
+    change_column :projects, :description, :string
+  end
+end
