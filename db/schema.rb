a2af6e0e3ed30dac104a60744ed8681584042742
Scott Silverman <scottsil@me.com>
2011-10-17 00:03:20 -0700

    change descriptions to text and remove 255 char limit

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111012084353) do
+ActiveRecord::Schema.define(:version => 20111017062316) do
 
   create_table "projects", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title"
-    t.string   "description"
+    t.text     "description"
     t.integer  "order_index"
     t.string   "video_id"
   end
